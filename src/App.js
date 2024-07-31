b4f57a3ab56c487a7e3e1b2b5bf7a8bfa94c1c22
carolbranquinho <carol.a.branquinho@gmail.com>
2024-07-31 12:17:28 -0300

    Add stepper

--- src/App.js
+++ src/App.js
@@ -1,61 +1,89 @@
 import './App.css';
 import { useState } from 'react';
-import { TextField, InputLabel, FormControl, Select, MenuItem, Container, Typography, Button } from '@mui/material';
+import { TextField, FormControl, Select, MenuItem, Container, Typography, Button, Stepper, Step, StepLabel, Box } from '@mui/material';
+
+const steps = ['Donation', 'Info', 'Payment'];
 
 function App() {
-  const [donationTier, setDonationTier] = useState()
+  const [donationTier, setDonationTier] = useState();
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
   return (
     <div className="App">
       <nav className='stepper-menu'>
-        
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            return (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
       </nav>
       <form className='form-container'>
-        <Container className='step-container step-1'>
-          <FormControl>
-            <Typography className='user-step-info'>Please choose the donation tier</Typography>
-            <InputLabel id="donation-tier">Donation tier</InputLabel>
-            <Select
-              labelId="donation-tier"
-              id="donation-tier-select"
-              value={donationTier}
-              label="Donation Tier"
-              onChange={(e) => setDonationTier(e.target.value)}
-            >
-              <MenuItem value={1}>Tier 1</MenuItem>
-              <MenuItem value={2}>Tier 2</MenuItem>
-              <MenuItem value={3}>Tier 3</MenuItem>
-            </Select>
-            <TextField id="donation-message" label="Leave a message with your Donation"/>
-          </FormControl>
-          <Container className='actions step-1'>
-            <Button>Next</Button>
+        {activeStep === 0 && (
+          <Container className='step-container step-1'>
+            <FormControl className='form'>
+              <Typography className='user-step-info'>Please choose the donation tier</Typography>
+              <Select
+                labelId="donation-tier"
+                id="donation-tier-select"
+                value={donationTier}
+                label="Donation Tier"
+                onChange={(e) => setDonationTier(e.target.value)}
+              >
+                <MenuItem value={1}>Tier 1</MenuItem>
+                <MenuItem value={2}>Tier 2</MenuItem>
+                <MenuItem value={3}>Tier 3</MenuItem>
+              </Select>
+              <TextField id="donation-message" label="Leave a message with your Donation" />
+            </FormControl>
+            <Container className='actions step-1'>
+              <Button onClick={handleNext}>Next</Button>
+            </Container>
           </Container>
-        </Container>
-        <Container className='step-container step-2'>
-          <FormControl>
-            <Typography className='user-step-info'>Provide your personal info</Typography>
-            <TextField id="name" label="Name" variant="standard" required/>
-            <TextField id="email" label="Email" variant="standard" type='email' required/>
-          </FormControl>
-          <Container className='actions step-2'>
-            <Button>Previous</Button>
-            <Button>Next</Button>
+        )}
+        {activeStep === 1 && (
+          <Container className='step-container step-2'>
+            <FormControl className='form'>
+              <Typography className='user-step-info'>Provide your personal info</Typography>
+              <TextField id="name" label="Name" variant="standard" required />
+              <TextField id="email" label="Email" variant="standard" type='email' required />
+            </FormControl>
+            <Container className='actions step-2'>
+              <Button onClick={handleBack}>Previous</Button>
+              <Button onClick={handleNext}>Next</Button>
+            </Container>
           </Container>
-        </Container>
-        <Container className='step-container step-3'>
-          <FormControl>
-            <TextField id="card-name" label="Cardholder Name" variant="standard" required/>
-            <TextField id="card-number" label="Card number" variant="standard" type='number' required/>
-            <TextField id="exp-date" label="Exp Date" variant="standard" type='date' required/>
-            <TextField id="cvv" label="CVV" variant="standard" type='password' required/>
-          </FormControl>
-          <Container className='actions step-3'>
-            <Button>Previous</Button>
-            <Button>Send</Button>
+        )}
+        {activeStep === 2 && (
+          <Container className='step-container step-3'>
+            <FormControl className='form'>
+              <TextField id="card-name" label="Cardholder Name" variant="standard" required />
+              <TextField id="card-number" label="Card number" variant="standard" type='number' required />
+              <TextField id="exp-date" label="Exp Date" variant="standard" type='date' required />
+              <TextField id="cvv" label="CVV" variant="standard" type='password' required />
+            </FormControl>
+            <Container className='actions step-3'>
+              <Button onClick={handleBack}>Previous</Button>
+              <Button >Send</Button>
+            </Container>
           </Container>
-        </Container>
+        )}
       </form>
-
     </div>
   );
 }
